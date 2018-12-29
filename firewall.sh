fc8c778080a6578bd6a9e54fc7317b1b0baf5e76
Alexander Jordan <JordanLAlexander@gmail.com>
2018-12-29 00:22:43 -0600

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-1>>/home/alex/fwlog
-2>>/home/alex/fwerr
+exec >> /home/alex/fwlog
+exec 2>&1
 
 #Drop all incoming packets
 /sbin/iptables -P INPUT DROP
@@ -8,20 +8,23 @@
 #Check to see if a rule exists in 1. If not, create.
 if 
         /sbin/iptables -R INPUT 1 -p tcp -m tcp -m multiport --dports 22,53,80,443 -j ACCEPT;
-then echo "Rule 1 updated"
+        then echo "%T: Rule 1 updated"
 else /sbin/iptables -A INPUT -p tcp -m tcp -m multiport --dports 22,53,80,443 -j ACCEPT
+        echo "%T: Added missing rule 1"
 fi
 
 #Check to see if a rule exists in 2. If not, create.
 if 
         /sbin/iptables -R INPUT 2 -p udp --dport 53 -j ACCEPT;
-then echo "Rule 2 updated"
+        then echo "%T: Rule 2 updated"
 else /sbin/iptables -A INPUT -p udp --dport 53 -j ACCEPT
+        echo "%T: Added missing rule 2"
 fi
 
 #Check to see if a rule exists in 3. If not, create.
 if 
-        /sbin/iptables -R INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT;
-then echo "Rule 3 updated"
+        /sbin/iptables -R INPUT 3 -m state --state RELATED,ESTABLISHED -j ACCEPT;
+        then echo "%T: Rule 3 updated"
 else /sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+        echo "%T: Added missing rule 3"
 fi
